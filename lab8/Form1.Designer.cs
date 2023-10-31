76ed63305223990f9925358a1f0b1b3bb9e9d0ae
Hiradoras <forwoermind@gmail.com>
2023-10-31 15:57:58 +0600

    Add project files.

---
+++ lab8/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button4 = new Button();
+            button5 = new Button();
+            checkBox4 = new CheckBox();
+            groupBox1 = new GroupBox();
+            button6 = new Button();
+            radioButton6 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton7 = new RadioButton();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(27, 22);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(93, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Select item 1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(27, 47);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(93, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Select item 2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(27, 72);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(93, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Select item 3";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 122);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 53);
+            button1.TabIndex = 3;
+            button1.Text = "Which Ones Are selected?";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(27, 181);
+            button2.Name = "button2";
+            button2.Size = new Size(188, 53);
+            button2.TabIndex = 4;
+            button2.Text = "check state property";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(27, 240);
+            button3.Name = "button3";
+            button3.Size = new Size(188, 53);
+            button3.TabIndex = 5;
+            button3.Text = "programmatically select";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(255, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(255, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(255, 72);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(244, 122);
+            button4.Name = "button4";
+            button4.Size = new Size(188, 53);
+            button4.TabIndex = 9;
+            button4.Text = "which ones are selected";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(244, 181);
+            button5.Name = "button5";
+            button5.Size = new Size(188, 53);
+            button5.TabIndex = 10;
+            button5.Text = "programmatically select";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(132, 313);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(138, 19);
+            checkBox4.TabIndex = 11;
+            checkBox4.Text = "check changed event";
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.CheckedChanged += checkBox4_CheckedChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(radioButton6);
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Location = new Point(445, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(260, 294);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(26, 159);
+            button6.Name = "button6";
+            button6.Size = new Size(188, 53);
+            button6.TabIndex = 13;
+            button6.Text = "which ones are selected";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(69, 100);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(94, 19);
+            radioButton6.TabIndex = 15;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "radioButton6";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(69, 75);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(94, 19);
+            radioButton5.TabIndex = 14;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "radioButton5";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(69, 50);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(94, 19);
+            radioButton4.TabIndex = 13;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(445, 322);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(137, 19);
+            radioButton7.TabIndex = 16;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "check chnaged event";
+            radioButton7.UseVisualStyleBackColor = true;
+            radioButton7.CheckedChanged += radioButton7_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(705, 368);
+            Controls.Add(radioButton7);
+            Controls.Add(groupBox1);
+            Controls.Add(checkBox4);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Button button4;
+        private Button button5;
+        private CheckBox checkBox4;
+        private GroupBox groupBox1;
+        private Button button6;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton7;
+    }
+}
